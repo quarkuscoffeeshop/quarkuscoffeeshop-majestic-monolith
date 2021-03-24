9cb636d720ef83de78e9074a11e42af15f2ccb61
jeremyrdavis <jeremy.davis@redhat.com>
2021-03-23 21:34:09 -0400

    tests passing

--- src/test/java/io/quarkuscoffeeshop/coffeeshop/web/CoffeeshopApiResourceTest.java
+++ src/test/java/io/quarkuscoffeeshop/coffeeshop/web/CoffeeshopApiResourceTest.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import io.quarkus.test.junit.QuarkusTest;
 import io.quarkuscoffeeshop.coffeeshop.TestUtils;
+import io.quarkuscoffeeshop.utils.JsonUtil;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
@@ -20,32 +21,16 @@ public class CoffeeshopApiResourceTest {
 
     Logger logger = LoggerFactory.getLogger(CoffeeshopApiResourceTest.class);
 
-    static ObjectMapper objectMapper;
-
-    @BeforeAll
-    public static void setUp() {
-        objectMapper = new ObjectMapper();
-        objectMapper.registerModule(new JavaTimeModule());
-    }
-
     @Test
     public void testPlaceOrder() {
 
-        try {
-
-            logger.info("Testing place order");
-            logger.info("PlaceOrderCommand: {}", objectMapper.writeValueAsString(TestUtils.mockPlaceOrderCommand()));
-
-            given()
-                    .contentType(MediaType.APPLICATION_JSON)
-                    .when()
-                    .body(objectMapper.writeValueAsString(TestUtils.mockPlaceOrderCommand()))
-                    .post("/api/order")
-                    .then()
-                    .statusCode(202);
-        } catch (JsonProcessingException e) {
-            assertNull(e, "Should not throw an Exception");
-        }
+        logger.info("Testing place order");
+        given()
+                .contentType(MediaType.APPLICATION_JSON)
+                .when()
+                .body(JsonUtil.toJson(TestUtils.mockPlaceOrderCommand()))
+                .then()
+                .statusCode(202);
 
     }
 }
